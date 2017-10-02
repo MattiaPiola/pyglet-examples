93f4aa40ca44226d496ac865c50d2a0f1185341b
MattiaPiola <mattia.piola@gmail.com>
2017-10-02 14:36:36 +0200

    Changed drawing algorithm. Still glitchy.

--- Perlin Noise/perlin_noise.py
+++ Perlin Noise/perlin_noise.py
@@ -2,24 +2,25 @@ from random import randint
 
 import pyglet as pyg
 from noise.perlin import SimplexNoise
-width = randint(400, 600)
-print(width)
-height = 25
+width = 600
+height = 250
 window = pyg.window.Window(resizable=True, width=width, height=height)
 noisee = SimplexNoise()
 colorsss = []
 
 
 def calculate_perlin(p_list):
-    rate = 50
-    for x in range(window.width):
-        for y in range(window.height):
+    rate = 250
+    for y in range(window.height):
+        column = []
+        for x in range(window.width):
             x_coord = x/rate+1901
             y_coord = y/rate+1901
             point = [('v2f', (x, y)), ("c3f", (noisee.noise2(x_coord, y_coord),
                                                noisee.noise2(x_coord, y_coord),
                                                noisee.noise2(x_coord, y_coord)))]
-            p_list.append(point)
+            column.append(point)
+        p_list.append(column)
 
 
 @window.event
@@ -29,10 +30,11 @@ def on_draw():
 
 @window.event()
 def on_mouse_press(x, y, button, modifiers):
-    for point in colorsss:
-        pyg.graphics.draw(1, pyg.gl.GL_POINTS, (point[0]), (point[1]))
+    for column in colorsss:
+    	for point in column:
+        	pyg.graphics.draw(1, pyg.gl.GL_POINTS, (point[0]), (point[1]))
 
-    pyg.image.get_buffer_manager().get_color_buffer().save('perlin_screenshot.png')
+    #pyg.image.get_buffer_manager().get_color_buffer().save('perlin_screenshot.png')
 
 
 pyg.app.run()
